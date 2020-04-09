9b886c386c82376225e1e5a9275244aba63e8d03
Thamiris Zalkowitsch <thamizalko@gmail.com>
2020-04-09 16:45:25 -0700

    add css and views

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
         views.post_list,
         name='post_list'
     ),
+    path(
+        'post/<int:pk>/',
+        views.post_detail,
+        name='post_detail',
+    )
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Post
@@ -13,3 +13,7 @@ def post_list(request):
     )
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, id=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
