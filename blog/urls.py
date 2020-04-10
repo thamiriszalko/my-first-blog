14f6b39785b456526bad97f5c5dbda589c55ad31
Thamiris Zalkowitsch <thamizalko@gmail.com>
2020-04-10 11:55:05 -0700

    Add views and buttons

--- blog/urls.py
+++ blog/urls.py
@@ -12,5 +12,15 @@ urlpatterns = [
         'post/<int:pk>/',
         views.post_detail,
         name='post_detail',
+    ),
+    path(
+        'post/new/',
+        views.post_new,
+        name='post_new',
+    ),
+    path(
+        'post/<int:pk>/edit/',
+        views.post_edit,
+        name='post_edit',
     )
 ]
